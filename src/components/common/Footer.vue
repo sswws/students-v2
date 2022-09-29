bc6f14cfc15e76dcebb8fbd762891243ca5d8b0c
sswws <han17772138808>
2022-09-29 21:10:14 +0800

    first commit

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    <el-card>
+      Frontend 2022 Casey
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
